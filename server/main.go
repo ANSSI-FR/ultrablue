f89f61bcf68bda0fcf2af546a4d541c7af97f823
Loic Buckwell <loic.buckwell@stagiaires.ssi.gouv.fr>
2022-06-29 13:19:14 +0200

    Delete existing characteristics
    In order to implement control-inversion, the server will make
    only one characteristic available to the clients, and choose what
    to send/receive onto it on interactions.
    Thus old characteristics are deleted, to prepare the introduction of
    the new unique characteristic.
    Currently, main.go only advertises a service, without any
    characteristic.
    
    Signed-off-by: Loic Buckwell <loic.buckwell@stagiaires.ssi.gouv.fr>

--- server/main.go
+++ server/main.go
@@ -1,34 +1,6 @@
 // SPDX-FileCopyrightText: 2022 ANSSI
 // SPDX-License-Identifier: Apache-2.0
 
-/*
-	ARCHITECTURE
-
-	Ultrablue is a client-server application, that operates over
-	Bluetooth Low Energy. This tool acts as the server.
-	Each step of the protocol is implemented in a characteristic,
-	and the client must read/write on those characteristic
-	successively to perform the remote attestation.
-	The protocol diagram can be found in the README.md file.
-
-	Characteristics implementation details:
-
-		- Each characteristic is declared in its own file, ending with Chr.go.
-
-		- As the chunking of the packets is not handled by the ble package,
-		each characteristic maintains its state in the context associated
-		with the connection.
-		It's up to the client to read/write enough times to complete the
-		operation, the server can't drive the communicatin and send every
-		chunks in a for loop.
-		TODO: the server should disconnect the client if it does not follow the expected steps of the protocol
-
-		- The r/w handlers of the characteristics runs in a goroutine, thus
-		errors/success are transmitted to the main routine through channels.
-
-	Note: The server only accepts one simulteanous client.
-*/
-
 // ultrablue-server
 package main
 
@@ -39,35 +11,91 @@ import (
 
 	"github.com/go-ble/ble"
 	"github.com/go-ble/ble/linux"
-	"github.com/go-ble/ble/linux/hci/evt"
 	"github.com/sirupsen/logrus"
 )
 
 // Command line arguments (global variables)
 var (
-	tpmPath  = flag.String("tpm", "/dev/tpm0", "Path of the TPM device to use")
 	enroll   = flag.Bool("enroll", false, "Must be true for a first time attestation")
-	loglevel = flag.Int("loglevel", 0, "Indicates the level of logging, 0 is the minimum, 2 is the maximum")
+	loglevel = flag.Int("loglevel", 0, "Indicates the level of logging, 0 is the minimum, 3 is the maximum")
+	mtu      = flag.Int("mtu", 500, "Set a custom MTU, which is basically the max size of the BLE packets")
 )
 
+/*
+	initLogger sets the level of logging
+	according to the loglevel parameter.
+	Here is a short description of the log
+	levels:
+		- 0: No log
+		- 1: Protocol steps logs
+		- 2: Debug messages
+		- 3: BLE packets trace
+*/
 func initLogger(loglevel int) {
 	switch loglevel {
 	case 1:
 		logrus.SetLevel(logrus.InfoLevel)
 	case 2:
 		logrus.SetLevel(logrus.DebugLevel)
+	case 3:
+		logrus.SetLevel(logrus.TraceLevel)
 	default:
 		logrus.SetLevel(logrus.ErrorLevel)
 	}
 }
 
-func onConnection(evt evt.LEConnectionComplete) {
-	logrus.Info("New device connection")
+/*
+   usage prints informations to guide the user at the
+   start of the program.
+*/
+func usage(erl bool) {
+	var yellow = "\033[33m"
+	var reset = "\033[0m"
+
+	if erl {
+		fmt.Println(yellow + `To enroll a new verifier device:
+
+   1. Install the Ultrablue application on your smartphone
+   2. From it, push the + button on the top-right corner
+   3. Scan the following QR code
+` + reset)
+	} else {
+		fmt.Println(yellow + `To perform the attestation:
+
+   1. Open the Ultrablue application on an enrolled device
+   2. Find this computer in the list of known attesters
+   3. Tap the play button
+` + reset)
+	}
 }
 
-func onDisconnection(evt evt.DisconnectionComplete) {
-	logrus.Info("Device disconnection")
-}
+/*
+	ARCHITECTURE
+
+	Ultrablue is a client-server application, that operates over
+	Bluetooth Low Energy. This tool acts as the server.
+	The client has the full control over the conversation, thus we
+	implemented some sort of control inversion, with go-routines,
+	channels and objects as follow:
+
+		- A characteristic is exposed and will handle the read/write
+		operations of the client and the chunking of the packets. It will
+		read/write on a message channel once an operation is complete.
+		The goal of the characteristic is to abstract the BLE transport layer.
+
+		- A protocol function that will implement the protocol logic. It will
+		be started as a go-routine, each time a new client read or write on the
+		characteristic for the first time.
+
+		- A state structure that contains everything the characteristic and the
+		protocol routines needs to know about the client connection to work
+		together.
+
+	NOTES:
+
+		- The server only accepts one simulteanous client.
+		- The protocol diagram can be found in the README.md file.
+*/
 
 func main() {
 	flag.Parse()
@@ -75,47 +103,16 @@ func main() {
 	initLogger(*loglevel)
 
 	logrus.Info("Opening the default HCI device")
-	device, err := linux.NewDevice(ble.OptConnectHandler(onConnection), ble.OptDisconnectHandler(onDisconnection))
+	device, err := linux.NewDevice()
 	if err != nil {
 		logrus.Fatal(err)
 	}
 	ble.SetDefaultDevice(device)
-	device.SetCentralRole()
 	defer device.Stop()
 
-	if *enroll {
-		logrus.Info("Generating registration QR code")
-
-		rbytes, err := getTPMRandom(32)
-		if err != nil {
-			logrus.Fatal(err)
-		}
-		key, iv := rbytes[0:16], rbytes[16:32]
-
-		mac := device.Address().String()
-
-		qrcode, err := generateRegistrationQR(key, iv, mac)
-		if err != nil {
-			logrus.Fatal(err)
-		}
-		fmt.Println(qrcode)
-	}
-
-	logrus.Info("Registering ultrablue service and characteristics")
+	logrus.Info("Registering ultrablue service and characteristic")
 	ultrablueSvc := ble.NewService(ultrablueSvcUUID)
-
-	errc := make(chan error)
-	rspc := make(chan int)
-
-	if *enroll {
-		ultrablueSvc.AddCharacteristic(registrationChr(errc))
-	}
-	ultrablueSvc.AddCharacteristic(authenticationChr(errc))
-	ultrablueSvc.AddCharacteristic(credentialActivationChr(errc))
-	ultrablueSvc.AddCharacteristic(attestationChr(errc))
-	ultrablueSvc.AddCharacteristic(responseChr(errc, rspc))
-
-	if err = ble.AddService(ultrablueSvc); err != nil {
+	if err := ble.AddService(ultrablueSvc); err != nil {
 		logrus.Fatal(err)
 	}
 
@@ -123,12 +120,18 @@ func main() {
 	ctx := ble.WithSigHandler(context.WithCancel(context.Background()))
 	go ble.AdvertiseNameAndServices(ctx, "Ultrablue server", ultrablueSvc.UUID)
 
+	if *enroll {
+		logrus.Info("Generating enrollment QR code")
+		addr := device.Address().String()
+		qrcode, err := generateQRCode(addr)
+		if err != nil {
+			logrus.Fatal(err)
+		}
+		fmt.Println(qrcode)
+	}
+
 	select {
 	case <-ctx.Done():
 		logrus.Fatal(ctx.Err())
-	case err := <-errc:
-		logrus.Fatal(err)
-	case <-rspc:
-		logrus.Info("Attestation succeeded")
 	}
 }
--- server/utils.go
+++ server/utils.go
@@ -4,72 +4,20 @@
 package main
 
 import (
-	"encoding/base64"
 	"fmt"
 
-	"github.com/google/go-tpm/tpm2"
 	"github.com/skip2/go-qrcode"
 )
 
 /*
-   usage prints informations to guide the user at the
-   start of the program.
+	generateQRCode generates a QR code containing the
+	string given as parameter, and returns it in an
+	ascii art string.
 */
-func usage(erl bool) {
-	var yellow = "\033[33m"
-	var reset = "\033[0m"
-
-	if erl {
-		fmt.Println(yellow + `To enroll a new verifier device:
-
-   1. Install the Ultrablue application on your smartphone
-   2. From it, push the + button on the top-right corner
-   3. Scan the following QR code
-` + reset)
-	} else {
-		fmt.Println(yellow + `To perform the attestation:
-
-   1. Open the Ultrablue application on an enrolled device
-   2. Find this computer in the list of known attesters
-   3. Tap the play button
-` + reset)
-	}
-}
-
-/*
-	getTPMRandom gets @size random bytes from the TPM.
-	This function is used to generate AES keys and IVs.
-*/
-func getTPMRandom(size uint16) ([]byte, error) {
-	rwc, err := tpm2.OpenTPM()
-	if err != nil {
-		return nil, err
-	}
-	defer rwc.Close()
-
-	rbytes, err := tpm2.GetRandom(rwc, size)
-	if err != nil {
-		return nil, err
-	}
-	return rbytes, nil
-}
-
-/*
-	generateRegistrationQR generates the QRcode the remote device needs to scan
-	to enroll itself. In the qrcode, there's:
-	- The AES key, base64 encoded
-	- The IV, base64 encoded
-	- The MAC address
-*/
-func generateRegistrationQR(key, iv []byte, mac string) (string, error) {
-
-	ek := base64.StdEncoding.EncodeToString(key)
-	ei := base64.StdEncoding.EncodeToString(iv)
-
-	qr, err := qrcode.New(fmt.Sprintf("%s\n%s\n%s\n", ek, ei, mac), qrcode.Low)
+func generateQRCode(data string) (string, error) {
+	qr, err := qrcode.New(fmt.Sprintf("%s\n", data), qrcode.Low)
 	if err != nil {
 		return "", err
 	}
-
 	return qr.ToString(false), nil
 }
