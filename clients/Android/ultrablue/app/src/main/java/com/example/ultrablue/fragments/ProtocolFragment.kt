e86b34f58319b8599a8de48eab0cd084d1862b28
Loic Buckwell <loic.buckwell@stagiaires.ssi.gouv.fr>
2022-07-18 16:54:11 +0200

    Allow to edit/delete a Device
    - On trash button click, a confirmation popup
    appears, and the device is deleted from the database
    on confirmation.
    - On device card click, the detail fragment appears.
    From it, you can now tap the pen icon to rename it.
    
    Signed-off-by: Loic Buckwell <loic.buckwell@stagiaires.ssi.gouv.fr>

--- clients/Android/ultrablue/app/src/main/java/com/example/ultrablue/fragments/ProtocolFragment.kt
+++ clients/Android/ultrablue/app/src/main/java/com/example/ultrablue/fragments/ProtocolFragment.kt
@@ -3,12 +3,32 @@ package com.example.ultrablue.fragments
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import com.example.ultrablue.MainActivity
 import com.example.ultrablue.R
+import com.example.ultrablue.database.Device
 
+/*
+    This fragment performs the Ultrablue attestation protocol.
+    It takes an optional Device parameter. If no Device is passed,
+    an enrollment is performed. Else, it means we already know about
+    the device to attest, and jump straight to the attestation part.
+ */
 class ProtocolFragment : Fragment() {
+    var device: Device? = null
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         setHasOptionsMenu(true)
+        device = requireArguments().getSerializable("device") as Device?
         return inflater.inflate(R.layout.fragment_device_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (requireActivity() as MainActivity).supportActionBar?.title = if (device != null) {
+            "Attestation in progress"
+        } else {
+            "Enrollment in progress"
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -17,6 +37,7 @@ class ProtocolFragment : Fragment() {
 
     override fun onPrepareOptionsMenu(menu: Menu) {
         menu.findItem(R.id.action_add).isVisible = false
+        menu.findItem(R.id.action_edit).isVisible = false
         super.onPrepareOptionsMenu(menu)
     }
 }
