e104e9e864fc99854292cb64b4053e81f39be448
Loic Buckwell <loic.buckwell@stagiaires.ssi.gouv.fr>
2022-07-21 16:53:21 +0200

    Use new menu API to set the action bar in each fragments
    Signed-off-by: Loic Buckwell <loic.buckwell@stagiaires.ssi.gouv.fr>

--- clients/Android/ultrablue/app/src/main/java/fr/gouv/ssi/ultrablue/fragments/DeviceFragment.kt
+++ clients/Android/ultrablue/app/src/main/java/fr/gouv/ssi/ultrablue/fragments/DeviceFragment.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.view.*
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toolbar
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import fr.gouv.ssi.ultrablue.database.DeviceViewModel
 import fr.gouv.ssi.ultrablue.MainActivity
@@ -18,8 +20,33 @@ class DeviceFragment : Fragment() {
     private var viewModel: DeviceViewModel? = null
     private var device: Device? = null
 
+    /*
+        Fragment lifecycle methods:
+     */
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        setHasOptionsMenu(true)
+        val menuHost = requireActivity()
+        menuHost.addMenuProvider(object: MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.action_bar, menu)
+            }
+            override fun onMenuItemSelected(item: MenuItem): Boolean {
+                return when (item.itemId) {
+                    // The + button has been clicked
+                    R.id.action_edit -> {
+                        showDeviceRenamingDialog()
+                        true
+                    }
+                    else -> false
+                }
+            }
+            override fun onPrepareMenu(menu: Menu) {
+                super.onPrepareMenu(menu)
+                menu.findItem(R.id.action_edit).isVisible = true
+                menu.findItem(R.id.action_add).isVisible = false
+            }
+        })
+        (activity as MainActivity).showUpButton()
         device = requireArguments().getSerializable("device") as Device
         return inflater.inflate(R.layout.fragment_device, container, false)
     }
@@ -27,50 +54,43 @@ class DeviceFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = (activity as MainActivity).viewModel
-        (requireActivity() as MainActivity).supportActionBar?.title = device?.name
-        displayDeviceInformations(view)
+        displayDeviceInformation(view)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.action_bar, menu)
+    override fun onDestroyView() {
+        super.onDestroyView()
+        (activity as MainActivity).hideUpButton()
     }
 
-    override fun onPrepareOptionsMenu(menu: Menu) {
-        menu.findItem(R.id.action_add).isVisible = false
-        super.onPrepareOptionsMenu(menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            // Displays an alert dialog, asking for the new device name.
-            R.id.action_edit -> {
-                val nameField = EditText(requireContext())
-                nameField.hint = "name"
-                nameField.width = 150
-                nameField.setPadding(30, 30, 30, 30)
-                val alertDialogBuilder = AlertDialog.Builder(activity)
-                alertDialogBuilder
-                    .setTitle(R.string.rename_device_dialog_title)
-                    .setView(nameField)
-                    .setPositiveButton("Ok") { _, _ ->
-                        device?.let {
-                            if (isNameValid(nameField.text.toString())) {
-                                renameDevice(it, nameField.text.toString())
-                            }
-                        }
-                    }
-                    .setNegativeButton("Cancel", null)
-                    .show()
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-    private fun displayDeviceInformations(view: View) {
+    // Sets the view components content with the device information.
+    private fun displayDeviceInformation(view: View) {
         val tv: TextView = view.findViewById(R.id.addr_value)
         tv.text = "${device?.addr}"
     }
+
+    private fun showDeviceRenamingDialog() {
+        val nameField = EditText(requireContext())
+        nameField.hint = "name"
+        nameField.width = 150
+        nameField.setPadding(30, 30, 30, 30)
+        val alertDialogBuilder = AlertDialog.Builder(activity)
+        alertDialogBuilder
+            .setTitle(R.string.rename_device_dialog_title)
+            .setView(nameField)
+            .setPositiveButton("Ok") { _, _ ->
+                device?.let {
+                    if (isNameValid(nameField.text.toString())) {
+                        renameDevice(it, nameField.text.toString())
+                    }
+                }
+            }
+            .setNegativeButton("Cancel", null)
+            .show()
+    }
+
+    /*
+        Fragment methods
+     */
 
     private fun isNameValid(name: String) : Boolean {
         return name.length in 4..12
@@ -79,6 +99,5 @@ class DeviceFragment : Fragment() {
     private fun renameDevice(dev: Device, name: String) {
         dev.name = name
         viewModel?.rename(dev, name)
-        (requireActivity() as MainActivity).supportActionBar?.title = dev.name
     }
 }
